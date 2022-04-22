44bdb9a189633d30c35447d9265c8780c4271bde
Aveek Saha <aveek.s98@gmail.com>
2022-04-22 09:53:49 +0530

    Download tweets

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,8 +1,11 @@
 import sys
 import csv
 import os
+import json
+from tqdm import tqdm
 
 dataset_path = "../FakeNewsNet/dataset"
+dump_location = "fakenewsnet_dataset"
 
 data_collection_choice = [
     {
@@ -39,16 +42,31 @@ def load_news_file(data_choice):
         except OverflowError:
             maxInt = int(maxInt / 10)
 
-    news_list = []
+    with open('{}/{}_{}.csv'.format(dataset_path, data_choice["news_source"],
+                                    data_choice["label"]), encoding="UTF-8") as csvfile:
+        lines = len(csvfile.readlines())
+
     with open('{}/{}_{}.csv'.format(dataset_path, data_choice["news_source"],
                                     data_choice["label"]), encoding="UTF-8") as csvfile:
         reader = csv.DictReader(csvfile)
-        for news in reader:
+
+        print("Collecting {} {}".format(data_choice["news_source"], data_choice["label"]))
+        for news in tqdm(reader, total=lines):
             # news_list.append(news, data_choice["label"], data_choice["news_source"])
             try:
                 tweets =  [int(tweet_id) for tweet_id in news["tweet_ids"].split("\t")]
-                print(tweets)
-            except:
+                for tweet in tweets:
+                    dump_dir = "{}/{}/{}/{}".format(dump_location, data_choice["news_source"], data_choice["label"], news["id"])
+                    tweet_dir = "{}/tweets".format(dump_dir)
+                    create_dir(dump_dir)
+                    create_dir(tweet_dir)
+
+                    os.system("snscrape --jsonl twitter-tweet {} > {}/{}.json".format(tweet, tweet_dir, tweet))
+
+            except Exception as e:
+                print("Error: ", e)
                 pass
 
-load_news_file(data_collection_choice[0])
+
+for data_choice in data_collection_choice:
+    load_news_file(data_choice)
