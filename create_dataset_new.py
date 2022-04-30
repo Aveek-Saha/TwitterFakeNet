19adaf88079f966d7594e1148ab45a310c0eed52
Aveek Saha <aveek.s98@gmail.com>
2022-04-30 20:22:02 +0530

    Organize data

---
+++ create_dataset_new.py
@@ -0,0 +1,68 @@
+import sys
+import csv
+import os
+import json
+from unicodedata import category
+
+from tqdm import tqdm
+
+from utils import *
+
+
+with open("{}/verified.txt".format(dump_location)) as fp:
+    lines = fp.readlines()
+
+userid_dict = {line.strip(): {"politifact_fake": [], "politifact_real": [
+], "gossipcop_fake": [], "gossipcop_real": []} for line in lines}
+
+total_tweets = []
+
+maxInt = sys.maxsize
+while True:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt / 10)
+
+for data_choice in data_collection_choice:
+    with open('{}/{}_{}.csv'.format(dataset_path, data_choice["news_source"],
+                                    data_choice["label"]), encoding="UTF-8") as csvfile:
+        lines = len(csvfile.readlines())
+
+    with open('{}/{}_{}.csv'.format(dataset_path, data_choice["news_source"],
+                                    data_choice["label"]), encoding="UTF-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        dump_dir = "{}/tweets".format(dump_location)
+
+        print("Collecting {} {}".format(
+            data_choice["news_source"], data_choice["label"]))
+        for news in tqdm(reader, total=lines):
+            # tweets = [int(tweet_id) for tweet_id in news["tweet_ids"].split(
+            #     "\t") if tweet_id.isdigit() if os.path.exists("{}/{}.json".format(dump_dir, tweet_id))]
+            # total_tweets += tweets
+            for tweet_id in news["tweet_ids"].split("\t"):
+                if (tweet_id.isdigit() and os.path.exists("{}/{}.json".format(dump_dir, tweet_id))):
+                    tweet = json.load(open("{}/{}.json".format(dump_dir, tweet_id), 'r'))
+                    user_id = tweet["user"]["id_str"]
+                    if user_id in userid_dict:
+                        data_category = "{}_{}".format(data_choice["news_source"], data_choice["label"])
+                        userid_dict[user_id][data_category].append(tweet_id)
+
+user_dict_present = {}
+
+for userid in userid_dict:
+    userid_dict[userid]["total_count"] = 0
+    for data_choice in data_collection_choice:
+        data_category = "{}_{}".format(data_choice["news_source"], data_choice["label"])
+        userid_dict[userid][data_category + "_count"] = len(userid_dict[userid][data_category])
+        userid_dict[userid]["total_count"] += len(userid_dict[userid][data_category])
+        # userid_dict[userid][data_category] = "\t".join(userid_dict[userid][data_category])
+    if userid_dict[userid]["total_count"] > 0:
+        user_dict_present[userid] = userid_dict[userid]
+        
+
+json.dump(user_dict_present, open(
+                "{}/user_map.json".format(dump_location), "w"))
